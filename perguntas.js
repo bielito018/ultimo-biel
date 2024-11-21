b97d8c372afa1ff736e335bad1cf7195fc67ad8d
bielito018 <cordeirogabriel482@gmail.com>
2024-11-21 15:20:38 -0300

    d

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'placa de video',
+    'rtx4060ti ou rtx4070 super?',
+    'rtx4070 super'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
